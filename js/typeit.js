f9cabe1e063fa9fc13a5be53ac874f3b01b6c300
Isaac <hawkins.isaac@gmail.com>
2017-04-28 14:30:39 -0500

    add typing function to header

---
+++ js/typeit.js
@@ -0,0 +1,6 @@
+$(document).ready(function(){
+	$('#brand-heading').typeIt({
+    lifeLike: true,
+	}).tiType("Hi! I'm Isaac.").tiPause(1000).tiDelete(14).tiType("I like creating web").tiBreak().tiType("sites").tiPause(300).tiDelete(5).tiType("apps").tiPause(300).tiDelete(4).tiType("experiences.").tiPause(500).tiDelete(32).tiType("Welcome!");
+     
+})
