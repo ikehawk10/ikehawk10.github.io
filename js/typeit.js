f3aaf8503ca35be53fe2ce96a7e9080194fc734a
Isaac <hawkins.isaac@gmail.com>
2017-04-29 01:58:53 -0500

    add skills animation

--- js/typeit.js
+++ js/typeit.js
@@ -1,6 +1,5 @@
 $(document).ready(function(){
 	$('#brand-heading').typeIt({
     lifeLike: true,
-	}).tiType("Hi! I'm Isaac.").tiPause(1000).tiDelete(14).tiType("I like creating web").tiBreak().tiType("sites").tiPause(300).tiDelete(5).tiType("apps").tiPause(300).tiDelete(4).tiType("experiences.").tiPause(500).tiDelete(32).tiType("Welcome!");
-     
-})
+	}).tiType("Hi!").tiPause(500).tiDelete(3).tiType("I'm Isaac").tiPause(1000).tiDelete(14).tiType("I create web").tiBreak().tiType("sites").tiPause(300).tiDelete(5).tiType("apps").tiPause(300).tiDelete(4).tiType("experiences").tiPause(500).tiDelete(25).tiType("Welcome!");
+});
