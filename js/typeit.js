358048d2d73315efbf4e9c26ac6231d65e3f87c2
Isaac <hawkins.isaac@gmail.com>
2017-05-16 16:21:38 -0500

    edit the typewriter text

--- js/typeit.js
+++ js/typeit.js
@@ -1,5 +1,5 @@
 $(document).ready(function(){
 	$('#brand-heading').typeIt({
     lifeLike: true,
-	}).tiType("Hi!").tiPause(500).tiDelete(3).tiType("I'm Isaac").tiPause(1000).tiDelete(14).tiType("I create web").tiBreak().tiType("sites").tiPause(300).tiDelete(5).tiType("apps").tiPause(300).tiDelete(4).tiType("experiences").tiPause(500).tiDelete(25).tiType("Welcome!");
+	}).tiType("Hi!").tiPause(700).tiDelete(3).tiType("I'm Isaac.").tiPause(700).tiDelete(10).tiType("Welcome to my page!");
 });
