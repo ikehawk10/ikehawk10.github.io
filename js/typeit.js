62ec7d1d12f4add5508af7e1f260dcd97fcea93d
Isaac <hawkins.isaac@gmail.com>
2017-05-23 19:14:00 -0500

    remove typeit animation and add simple fadein animation

--- js/typeit.js
+++
@@ -1,5 +0,0 @@
-$(document).ready(function(){
-	$('#brand-heading').typeIt({
-    lifeLike: true,
-	}).tiType("Hi!").tiPause(700).tiDelete(3).tiType("I'm Isaac.").tiPause(700).tiDelete(10).tiType("Welcome to my page!");
-});
